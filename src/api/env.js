9248d64802522ecab2af43295f48d4374b5be5e1
guo <1014512443@qq.com>
2019-06-05 22:22:30 +0800

    commit

---
+++ src/api/env.js
@@ -0,0 +1,12 @@
+var bsaeUrl = '';
+let routerMode = 'hash'
+
+if (process.env.NODE_ENV == 'development') {
+  baseUrl = ''
+} else if (process.env.NODE_ENV == 'production') {
+  baseUrl = 'http://47.101.130.9'
+}
+export default {
+  bsaeUrl,
+  routerMode
+}
