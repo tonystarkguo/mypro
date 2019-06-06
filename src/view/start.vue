b124416e21948630485dcc397c729ee0aea4a19e
hui.guo <hui.guo@wm-holding.com.cn>
2019-06-06 18:25:00 +0800

    微调

--- src/view/start.vue
+++ src/view/start.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="start">
         <div class="bgdiv"></div>
-        <div class="strContent"><p class="first-words">{{test}}</p></div>
+        <div class="strContent">
+          <div class="strList" v-for="(item,index) in aside " :key=index >
+          <p class="first-words">
+            {{item.test}}
+          </p><address class="first-words">{{item.author}}</address>
+          </div>
+        </div>
     </div>
 </template>
 <script>
@@ -9,7 +15,7 @@ export default {
   name: 'start',
   data () {
     return {
-      test: "你是惊蛰的一声蝉鸣，苏醒了忽明忽暗的云。你是六月的柠，青涩了整个夏天。你是江南的杏花，也是我唯一的归途，更是我荒野辽阔的星，十七岁许愿的石子，画卷最后一抹的红，醒木里那个相忘江湖最后成为执念的她。 ——出自网易云热评《起风了》"
+      aside: [{test: '你是惊蛰的一声蝉鸣，苏醒了忽明忽暗的云。你是六月的柠，青涩了整个夏天。你是江南的杏花，也是我唯一的归途，更是我荒野辽阔的星，十七岁许愿的石子，画卷最后一抹的红，醒木里那个相忘江湖最后成为执念的她。', author: '——出自网易云热评《起风了》'}]
     }
   }
 }
@@ -45,6 +51,10 @@ export default {
       width: 100%;
       color:white;
       font-size: 22px;
+      .strList{
+        height: 100%;
+        width: 100%;
+      }
       p{
         margin-left: auto;
         margin-right: auto;
@@ -52,10 +62,13 @@ export default {
         text-justify: inter-word;
         text-transform: capitalize;
         writing-mode: vertical-lr;
-        line-height: 55px;
+        /*line-height: 55px;*/
+        /*line-height:15vw;*/
         letter-spacing: 5px;
         height: 90%;
         margin-top: 10%;
+        display: inline-block;
+        text-indent:20px
       }
       .first-words {
         opacity: 0;
@@ -63,7 +76,23 @@ export default {
         /*规定动画的最后状态为结束状态*/
         animation-fill-mode: forwards;
       }
-
+      address{
+        writing-mode: vertical-lr;
+        display: inline-block;
+        height: 100%;
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+        text-justify: inter-word;
+        text-transform: capitalize;
+        -webkit-writing-mode: vertical-lr;
+        -ms-writing-mode: tb-lr;
+        writing-mode: vertical-lr;
+        /*line-height: 55px;*/
+        letter-spacing: 5px;
+        height: 90%;
+        margin-top: 10%;
+      }
     }
     }
 </style>
