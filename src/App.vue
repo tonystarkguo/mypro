be3814a3873f50c397c95613afcb41aac22f99c2
guo <1014512443@qq.com>
2019-06-16 21:41:50 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,8 @@ html,body{
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  height: 100%;
+  height: 100vh;
+  min-height: 100%;
   width: 100%;
   /*margin-top: 60px;*/
 }
